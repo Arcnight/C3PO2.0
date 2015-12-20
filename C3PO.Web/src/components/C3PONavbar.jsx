41cfd0eecb9173bd1ede2886311cdda535d22ee4
Rufus Knight <roknight@arcnight.com>
2015-12-20 17:50:02 -0500

    Updated C3PO navbar

--- C3PO.Web/src/components/C3PONavbar.jsx
+++ C3PO.Web/src/components/C3PONavbar.jsx
@@ -9,31 +9,31 @@ var React = require('react');
 var Navbar = React.createClass({
     render() {
         return (
-            <div className="ui menu">
+            <div className="ui top attached menu">
                 <div className="header item">CEBOSS</div>
                 <a className="item active">Home</a>
                 <a className="item">Boarding</a>
-                <div className="item">
+                <div className="ui dropdown item">
                     Search
                     <div className="menu">
-                        <a className="item">Basic Search</a>
-                        <a className="item">Dhow Search</a>
-                        <a className="item">Map Search</a>
+						<a className="item">Basic Search</a>
+						<a className="item">Dhow Search</a>
+						<a className="item">Map Search</a>
                     </div>
                 </div>
-                <div className="item">
+                <div className="ui dropdown item">
                     Administration
-                    <div className="item">
-                        <a className="item">User Management</a>
-                        <a className="item">Metrics</a>
-                        <a className="item">Templates</a>
+                    <div className="menu">
+						<a className="item">User Management</a>
+						<a className="item">Metrics</a>
+						<a className="item">Templates</a>
                     </div>
                 </div>
-                <div className="item">
+                <div className="ui dropdown item">
                     System
                     <div className="menu">
-                        <a className="item">Links</a>
-                        <a className="item">Status</a>
+						<a className="item">Links</a>
+						<a className="item">Status</a>
                     </div>
                 </div>
                 <a className="item">Metrics</a>
